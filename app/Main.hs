73a337a1169194c36f88feb51ac9646486f97b47
Peter Murphy <pete@simspace.com>
2020-08-09 01:08:08 -0400

    Paused at 1:28:56

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,37 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
+import qualified Data.Text.Lazy as TL
+import GHC.TypeNats
 import qualified Graphics.Vty as Vty
 import Lib
-import qualified Data.Text.Lazy as TL
 
 helloWorld :: Component ()
 helloWorld = Component \_ ->
   Vty.picForImage <$> renderText "Hello, world"
 
-newtype Name = Name TL.Text
+newtype Name (p :: Nat) = Name TL.Text
 
 sayHello :: Component ()
 sayHello = Component \_ -> do
-  Name name <- useContextWithDefault (Name "stranger")
-  Vty.picForImage <$> renderText ("Hello, " <> name)
+  Name n0 <- useContextWithDefault (Name @1 "stranger")
+  Name n1 <- useContextWithDefault (Name @2 "stranger")
+  Vty.picForImage <$> renderText ("Hello, " <> n0 <> " and " <> n1)
 
 sayHelloTo :: Component TL.Text
 sayHelloTo = Component \name -> do
-  withContext (Name name) (runComponent sayHello ())
+  withContext (Name @1 name)
+    (withContext (Name @2 "Stacy") (runComponent sayHello ()))
+
+timer :: Component ()
+timer = Component \_ -> do
+  (counter, setCounter) <- useState (0 :: Int)
+  Vty.picForImage <$> renderText ("Counter: " <> TL.pack (show counter))
 
 main :: IO ()
-main = render sayHelloTo "Bob"
+main = render timer ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,73 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Lib where
 
 import Control.Monad.Reader
+import Control.Monad.State
+import Data.Function
 import Data.Maybe
 import qualified Data.TMap as TM
+import qualified Data.Text.Lazy as TL
 import Data.Typeable
 import qualified Graphics.Vty as Vty
-import qualified Data.Text.Lazy as TL
+import Control.Concurrent.STM.TVar
+import Control.Concurrent
+import Control.Monad.STM
 
 newtype React a = React
   { runReact :: ReaderT TM.TMap IO a
   }
-  deriving newtype (Functor, Applicative, Monad, MonadReader TM.TMap)
+  deriving newtype
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadReader TM.TMap
+    )
 
 newtype Component props = Component
   { runComponent :: props -> React Vty.Picture
   }
 
+newtype StateMap = StateMap (TVar TM.TMap)
+
+-- | Context
 withContext :: Typeable ctx => ctx -> React a -> React a
 withContext ctx = local (TM.insert ctx)
 
 useContext :: Typeable a => React (Maybe a)
-useContext = do
-  asks TM.lookup
+useContext = asks TM.lookup
 
 useContextWithDefault :: Typeable a => a -> React a
 useContextWithDefault def = do
   fromMaybe def <$> asks TM.lookup
 
+-- | State
+useState :: forall s. Typeable s => s -> React (s, s -> IO ())
+useState def = do
+  StateMap stateMap <- fromJust <$> useContext
+  s <- React (liftIO (fromMaybe def . TM.lookup <$> readTVarIO stateMap))
+  pure (s, setState stateMap)
+  where
+    setState :: TVar TM.TMap -> s -> IO ()
+    setState var s = atomically (modifyTVar' var (TM.insert s))
+
+-- | Effect
+useEffect :: IO () -> React ()
+useEffect = void . React . liftIO . forkIO
+
 render :: Component props -> props -> IO ()
 render (Component renderComponent) props = do
   vty <- Vty.mkVty Vty.defaultConfig
-  pic <- flip runReaderT mempty (runReact (renderComponent props))
+  stateMap <- newTVarIO mempty
+  pic <- renderComponent props
+    & withContext (StateMap stateMap)
+    & runReact
+    & flip runReaderT mempty
   Vty.update vty pic
   getLine
   Vty.shutdown vty
